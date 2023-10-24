2001511cb1bfece2542d902788a330182a18283a
simonelopess <sisilopes20@gmail.com>
2023-10-24 19:38:39 -0300

    feat: add home screen

---
+++ src/components/Logo.tsx
@@ -0,0 +1,8 @@
+import logoImg from "../assets/Logo.svg";
+import styles from "./Logo.module.css";
+
+function LogoComponent() {
+  return <img src={logoImg} className={styles.logo} alt="Todo App" />;
+}
+
+export default LogoComponent;
